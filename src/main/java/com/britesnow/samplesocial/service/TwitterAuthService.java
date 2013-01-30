76a71713bd655d08245be8de20af345699af36e8
Liu Xian <liuxianmail@gmail.com>
2013-01-30 19:17:20 +0800

    fix the network issue

--- src/main/java/com/britesnow/samplesocial/service/TwitterAuthService.java
+++ src/main/java/com/britesnow/samplesocial/service/TwitterAuthService.java
@@ -1,11 +1,13 @@
 package com.britesnow.samplesocial.service;
 
 import java.util.Map;
+import java.util.Scanner;
 
 import javax.inject.Inject;
 
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.builder.api.TwitterApi;
+import org.scribe.model.Token;
 import org.scribe.oauth.OAuthService;
 
 import com.britesnow.snow.web.binding.ApplicationProperties;
@@ -17,19 +19,18 @@ public class TwitterAuthService{
     @ApplicationProperties
     private Map cfg;
 	
-//    public String getAuthorizationUrl() {
-//        OAuthService service = new ServiceBuilder().provider(TwitterApi.class).apiKey(getApiKey()).apiSecret(getApiSecret()).callback(getCallBackUrl()).build();
-////        Token requestToken = service.getRequestToken();
-//        String authorizationUrl = service.getAuthorizationUrl(null);
-//        System.out.println(authorizationUrl);
-//        return authorizationUrl;
-//    }
     
     public String getAuthorizationUrl() {
-    	 OAuthService service = new ServiceBuilder().provider(TwitterApi.class).apiKey(getApiKey()).apiSecret(getApiSecret()).callback(getCallBackUrl()).build();
-    	 String authorizationUrl = service.getAuthorizationUrl(null);
-    	 System.out.println(authorizationUrl);
-    	 return authorizationUrl;
+    	OAuthService service = new ServiceBuilder()
+        	.provider(TwitterApi.class)
+        		.apiKey("6icbcAXyZx67r8uTAUM5Qw")
+        			.apiSecret("SCCAdUUc6LXxiazxH3N0QfpNUvlUy84mZ2XZKiv39s")
+        				.build();
+		
+		Token requestToken = service.getRequestToken();
+		String url = service.getAuthorizationUrl(requestToken);
+		return url;
+    	
     }
     
     public String getApiKey() {
