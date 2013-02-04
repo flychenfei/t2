f7345f9d29157049c1d9179da3f89c37051e861f
flymyao <flymyao@gmail.com>
2013-02-04 18:00:06 +0800

    +add function to add user email

--- src/main/webapp/js/GithubUserInfo.js
+++ src/main/webapp/js/GithubUserInfo.js
@@ -2,6 +2,13 @@
 	brite.registerView("GithubUserInfo",{emptyParent:true},{
 		create:function(data,config){
 			return  app.render("tmpl-GithubUserInfo",{userInfo:data.userInfo,emails:data.emails});
+		},
+		events:{
+			"click; .btn":function(event){
+				app.githubApi.addEmail({email:$(":input[name='email']").val()}).pipe(function(email){
+					alert(email+" has been added.")
+				});
+			}
 		}
 	})
 })();
