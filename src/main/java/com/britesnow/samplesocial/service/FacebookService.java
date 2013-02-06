2a35627885be7222b0251ab87e2eb63f95e8b968
lizhe5 <southgate@qq.com>
2013-02-06 14:35:56 +0800

    . refactor FacebookService api

--- src/main/java/com/britesnow/samplesocial/service/FacebookService.java
+++ src/main/java/com/britesnow/samplesocial/service/FacebookService.java
@@ -2,6 +2,7 @@ package com.britesnow.samplesocial.service;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -16,57 +17,54 @@ import com.restfb.Facebook;
 import com.restfb.Parameter;
 import com.restfb.json.JsonObject;
 import com.restfb.types.FacebookType;
+import com.restfb.types.Page;
 import com.restfb.types.User;
 
 public class FacebookService {
     public User getMyInformation(String accessToken) {
-        User user = new DefaultFacebookClient(accessToken).fetchObject("me", User.class);
-        return user;
+        return getUserInformation(accessToken, "me");
     }
 
-    public User getFriendInformation(String accessToken, String userId) {
+    public User getUserInformation(String accessToken, String userId) {
         User user = new DefaultFacebookClient(accessToken).fetchObject(userId, User.class);
         return user;
     }
 
     public List getAllFriends(String accessToken) {
-        Connection<User> myFriends = new DefaultFacebookClient(accessToken).fetchConnection("me/friends", User.class);
-        return myFriends.getData();
+        return getUserAllFriends(accessToken, "me");
+    }
+
+    public List getUserAllFriends(String accessToken, String userId) {
+        Connection<User> friends = new DefaultFacebookClient(accessToken).fetchConnection(userId + "/friends", User.class);
+        return friends.getData();
     }
 
     public List getFriendsByPage(String accessToken, String query, Integer limit, Integer offset) {
-        // if (limit == null || limit <= 0) {
-        // limit = 10;
-        // }
-        // if (offset == null || offset < 0) {
-        // offset = 0;
-        // }
-        // Connection<User> myFriends = null;
-        // if (StringUtils.isNotBlank(query)) {
-        // myFriends = new DefaultFacebookClient(accessToken).fetchConnection("me/friends", User.class,
-        // Parameter.with("q", query), Parameter.with("limit", limit), Parameter.with("offset", offset));
-        // } else {
-        // myFriends = new DefaultFacebookClient(accessToken).fetchConnection("me/friends", User.class,
-        // Parameter.with("limit", limit), Parameter.with("offset", offset));
-        // }
-        // return myFriends.getData();
         String fql = "SELECT name,uid,email FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())";
         if (StringUtils.isNotBlank(query)) {
             fql += " and name='" + query + "'";
         }
         List<FqlUser> users = new DefaultFacebookClient(accessToken).executeQuery(fql, FqlUser.class);
         return users;
-
     }
 
-    public String publish(String accessToken, String userId, String message) {
-        FacebookType publishMessageResponse = new DefaultFacebookClient(accessToken).publish(userId + "/feed", FacebookType.class, Parameter.with("message", message));
+    public String publish(String accessToken, String type, String userId, String message) {
+        FacebookType publishMessageResponse = new DefaultFacebookClient(accessToken).publish(userId + "/" + type, FacebookType.class, Parameter.with("message", message));
         return publishMessageResponse.getId();
+    }
+
+    public String publishFeed(String accessToken, String type, String userId, String message) {
+        return publish(accessToken, "feed", userId, message);
     }
 
     public String publishPhoto(String accessToken, String userId, String message, InputStream is) {
         FacebookType publishPhotoResponse = new DefaultFacebookClient(accessToken).publish(userId + "/photos", FacebookType.class, BinaryAttachment.with("userId", is), Parameter.with("message", message));
         return publishPhotoResponse.getId();
+    }
+
+    public boolean deleteObject(String accessToken, String messageId) {
+        boolean result = new DefaultFacebookClient(accessToken).deleteObject(messageId);
+        return result;
     }
 
     public List getFeedList(String accessToken, String userId, String type, Integer limit, Integer offset) {
@@ -147,4 +145,13 @@ public class FacebookService {
             this.hometownname = hometownname;
         }
     }
+
+    public static class FetchObjectsResults {
+        @Facebook
+        User me;
+
+        @Facebook(value = "cocacola")
+        Page page;
+    }
+
 }
