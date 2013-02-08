5c13c9b6ce4bae7fd757daec00dc305e93092869
flymyao <flymyao@gmail.com>
2013-02-08 11:17:10 +0800

    +add app.githubApi.js

---
+++ src/main/webapp/js/app.githubApi.js
@@ -0,0 +1,40 @@
+var app = app || {};
+(function(){
+	app.githubApi={
+        showUserInfo : function(){
+        	 var params = {};
+        	 params.method = "Get";
+        	return app.getJsonData(contextPath + "/github/userInfo",params);
+        },
+        addEmail:function(opts){
+        	var params = opts||{};
+        	params.method = "Post";
+        	return app.getJsonData(contextPath + "/github/addEmail",params);
+        },
+        deleteEmail:function(opts){
+        	var params = opts||{};
+        	params.method = "Post";
+        	return app.getJsonData(contextPath + "/github/deleteEmail",params);
+        },
+        getRepositories:function(){
+        	var params = {};
+        	params.method = "Get";
+        	return app.getJsonData(contextPath + "/github/repositories",params);
+        },
+        createRepository:function(opts){
+        	var params = opts||{};
+        	params.method = "Post";
+        	return app.getJsonData(contextPath + "/github/createRepository",params);
+        },
+        editRepository:function(opts){
+        	var params = opts||{};
+        	params.method="Post";
+        	return app.getJsonData(contextPath+"/github/editRepository",params);
+        },
+        getCommits:function(opts){
+        	var params = opts||{};
+        	params.method="Get";
+        	return app.getJsonData(contextPath+"/github/getCommits",params);
+        }
+    };
+})();
