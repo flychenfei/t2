8a9981334944e5fb9794efd53e18a660f53a0b64
flymyao <flymyao@gmail.com>
2013-02-20 15:29:36 +0800

    +add loading message when loading file

--- src/main/webapp/js/DropboxFiles.js
+++ src/main/webapp/js/DropboxFiles.js
@@ -6,9 +6,13 @@
 		events:{
 			"click;.pointer":function(event){
 				var path = $(event.target).closest("tr").attr("data-path");
+				var isDir = $(event.target).closest("tr").attr("data-dir");
+				if("true"==isDir)
+					$(".loading").toggleClass("hide");
 				app.dropboxApi.getMetadata({path:path}).pipe(function(metadata){
 					metadata = metadata.result;
 					brite.display("DropboxFiles",$(".tab-content"),{metadata:metadata});
+					$(".loading").toggleClass("hide");
 				});
 			}
 		}
