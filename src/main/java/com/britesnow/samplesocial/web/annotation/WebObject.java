9748d3e1ec7772d32615a361030ac0769d0e7616
woofgl <woofgl@gmail.com>
2013-02-25 09:44:38 +0800

    - fix webobject issue

--- src/main/java/com/britesnow/samplesocial/web/annotation/WebObject.java
+++ src/main/java/com/britesnow/samplesocial/web/annotation/WebObject.java
@@ -1,5 +1,7 @@
 package com.britesnow.samplesocial.web.annotation;
 
+import com.google.inject.BindingAnnotation;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,6 +9,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
+@BindingAnnotation()
 public @interface WebObject {
     String prefix() default "";
 }
