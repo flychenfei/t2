db37e31dd2bf8f343899d5702cbca6d0e707ca1a
flymyao <flymyao@gmail.com>
2013-03-05 15:49:01 +0800

    +add to get file content under repository

--- src/main/webapp/js/GithubDialog.js
+++ src/main/webapp/js/GithubDialog.js
@@ -9,6 +9,14 @@
 			},
 			"click;.btn.cancel":function(event){
 				this.$el.remove();
+			},
+			"click;.filename":function(event){
+				var current = $(event.target);
+				var path=$(current).closest("span.filename").attr("data-path");
+				var repo = $(current).closest("div.files").attr("data-repo");
+				app.githubApi.getContents({repo:repo,path:path}).pipe(function(files){
+					console.log(files);
+				});
 			}
 		}
 	})
