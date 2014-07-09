781d2b22273a402b85939624ece01e7b99c5b44c
flymyao <flymyao@gmail.com>
2014-07-09 18:16:59 +0800

    .[GoogleDrive] implement google drive file upload api

--- src/main/java/com/britesnow/samplesocial/service/GoogleDocsService.java
+++ src/main/java/com/britesnow/samplesocial/service/GoogleDocsService.java
@@ -7,15 +7,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.fileupload.FileItem;
+
 import com.britesnow.snow.util.Pair;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpMediaType;
 import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.InputStreamContent;
+import com.google.api.client.http.MultipartContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.http.json.JsonHttpContent;
+import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.Drive.Files;
+import com.google.api.services.drive.Drive.Files.Insert;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
 import com.google.inject.Inject;
@@ -27,6 +35,9 @@ public class GoogleDocsService {
     @Inject
     private GoogleAuthService authService;
     
+    private static String MEDIATYPE = "https://www.googleapis.com/upload/drive/v2/files?uploadType=media";
+    private static String MULTIPARTTYPE = "https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart";
+//    private static String RESUMABLETYPE = "https://www.googleapis.com/upload/drive/v2/files?uploadType=resumable";
     
     public Pair<String, List<Map>> searchFile(String title,String nextPagetoken, Integer pageSize){
     	List<Map> results = new ArrayList<Map>();
@@ -115,7 +126,86 @@ public class GoogleDocsService {
         return new Pair<String, List<Map>>(filelist.getNextPageToken(), results);
     }
     
-    //Move a file to the trash.
+    public boolean uploadFile(FileItem fileItem){
+    	if(fileItem.getSize() > 1000){
+    		return multipartUploadFile(fileItem);
+    	}
+    	if(fileItem.getName() == null){
+    		return simpleUploadFile(fileItem);
+    	}
+    	File metadata = new File();
+    	metadata.setTitle(fileItem.getName());
+    	metadata.setMimeType(fileItem.getContentType());
+        try {
+            InputStreamContent content = new InputStreamContent("application/vnd.google-apps.drive-sdk", fileItem.getInputStream());
+            Insert insert = getDriverService().files().insert(metadata, content);
+            insert.put("uploadType", "multipart");
+            insert.execute();
+          return true;
+        } catch (IOException e) {
+          e.printStackTrace();
+          return false;
+        }
+    }
+    
+    /**
+     * Just upload a media without metadata 
+     * uploadType=media
+     * @return
+     */
+    public boolean simpleUploadFile(FileItem fileItem){
+    	
+    	 try {
+             InputStreamContent content = new InputStreamContent(fileItem.getContentType(), fileItem.getInputStream());
+             Drive drive = getDriverService();
+             GenericUrl url = new GenericUrl(MEDIATYPE);
+             drive.getRequestFactory().buildPostRequest(url, content).execute();
+           return true;
+         } catch (IOException e) {
+           e.printStackTrace();
+           return false;
+         }
+    }
+    
+    /**
+     * Upload a media with metadata 
+     * uploadType=multipart
+     * @return
+     */
+    public boolean multipartUploadFile(FileItem fileItem){
+
+        try {
+        	MultipartContent content = new MultipartContent();
+        	content.setMediaType(new HttpMediaType("multipart/related"));
+        	content.setBoundary("__END_OF_PART__");
+        	//take file metadata with json
+        	JsonFactory jsonFactory = new JacksonFactory();
+        	Map<String,String> metadata = new HashMap<String,String>();
+        	metadata.put("title", fileItem.getName());
+        	JsonHttpContent jhc = new JsonHttpContent(jsonFactory, metadata);
+        	jhc.setMediaType(new HttpMediaType("application/json"));
+        	//file content
+        	InputStreamContent fileContent = new InputStreamContent(fileItem.getContentType(), fileItem.getInputStream());
+        	//add MultipartContent
+        	content.addPart(new MultipartContent.Part(jhc));
+        	content.addPart(new MultipartContent.Part(fileContent));
+        	
+            Drive drive = getDriverService();
+            GenericUrl url = new GenericUrl(MULTIPARTTYPE);
+            drive.getRequestFactory().buildPostRequest(url, content).execute();
+          return true;
+        } catch (IOException e) {
+          e.printStackTrace();
+          return false;
+        }
+    }
+    
+    /**
+     * Move the file to the trash
+     * @param fileId
+     * @param Permanent
+     * @return
+     */
     public boolean trashFile(String fileId,  boolean Permanent){
     	if(Permanent){
     		return deleteFile(fileId);
@@ -129,7 +219,11 @@ public class GoogleDocsService {
         return true;
     }
     
-    //Permanently delete a file, skipping the trash.
+    /**
+     * Permanently delete the file, skipping the trash
+     * @param fileId
+     * @return
+     */
     public boolean deleteFile(String fileId){
     	Drive service = getDriverService();
 		try {
@@ -151,7 +245,6 @@ public class GoogleDocsService {
     	        return null;
     	      }
     	    } else {
-    	      // The file doesn't have any content stored on Drive.
     	      return null;
     	    }
 	}
