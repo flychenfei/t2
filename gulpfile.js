94910b5ae75c051526168278518b93bdab1321b9
flymyao <flymyao@gmail.com>
2015-09-23 14:42:33 +0800

    #175 first pass at adding node.js for web files processing

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var hbsp = require("hbsp");
+var vdev = require("vdev");
+var path = require("path");
+var concat = require('gulp-concat');
+var gulp = require("gulp");
+var less = require("gulp-less");
+var del = require("del");
+var fs = require("fs");
+
+
+var hbsPrecompile = hbsp.precompile;
+
+var dbPrefix = "samplesocial";
+var webappDir = "src/main/webapp/";
+var sqlDir = "src/main/webapp/WEB-INF/sql/";
+
+var cssDir = path.join(webappDir,"/css/");
+
+gulp.task('default',['clean', 'hbs', 'less']);
+
+// --------- Web Assets Processing --------- //
+gulp.task('watch', ['default'], function(){
+    gulp.watch(path.join(webappDir,"/tmpl/",'*.tmpl'), ['hbs']);
+
+    gulp.watch(path.join(webappDir,"/less/",'*.less'), ['less']);
+
+});
+
+gulp.task('clean', function(){
+    var dirs = [commonCssDir, cssDir, sysadminCssDir];
+
+    // make sure the directories exists (they might not in fresh clone)
+    var dir;
+    for (var i = 0; i < dirs.length ; i ++){
+        dir = dirs[i];
+        if (!fs.existsSync(dir)) {
+            fs.mkdir(dir);
+        }
+        del.sync(dir + "*.css");
+    }
+});
+
+gulp.task('hbs', function() {
+    gulp.src(path.join(webappDir,"/tmpl/",'*.tmpl'))
+        .pipe(hbsPrecompile())
+        .pipe(concat("templates.js"))
+        .pipe(gulp.dest(path.join(webappDir,"/js/")));
+});
+
+gulp.task('less', function() {
+    gulp.src(path.join(webappDir,"/less/",'*.less'))
+        .pipe(less())
+        .pipe(gulp.dest(cssDir));
+});
+// --------- /Web Assets Processing --------- //
+
+gulp.task('recreateDb', function(){
+    vdev.psql("postgres", null, "postgres", vdev.listSqlFiles(sqlDir,{to:0}));
+    vdev.psql(dbPrefix + "_user", null, dbPrefix + "_db", vdev.listSqlFiles(sqlDir,{from:1}));
+});
