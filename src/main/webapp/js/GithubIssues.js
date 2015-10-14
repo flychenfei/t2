a6536d350eff265d122db6667b9364eca70b864d
JianweiYu <jianwei.yu.ch@gmail.com>
2015-10-14 17:42:14 +0800

    #190 [Github] Separately lists the closed and open issues

--- src/main/webapp/js/GithubIssues.js
+++ src/main/webapp/js/GithubIssues.js
@@ -1,7 +1,31 @@
 (function(){
 	brite.registerView("GithubIssues",{emptyParent:true},{
 		create:function(data,config){
-			return app.render("tmpl-GithubIssues",{issues:data.issues,name:data.name,login:data.login});
+			return app.render("tmpl-GithubIssues",{issues:data.issues,name:data.name,login:data.login,issueState:data.issueState});
+		},
+		events:{
+			"click;.openIssues":function(event){
+				var name = $(event.target).closest("div").attr("data-name");
+				var login = $(event.target).closest("div").attr("data-login");
+				app.githubApi.getIssues({
+					name:name,
+					login:login,
+					state:"open"
+				}).pipe(function(json){
+					brite.display("GithubIssues",$(".tab-content"),{issues:json.result,name:name,login:login,issueState:"open"});
+				});
+			},
+			"click;.closedIssues":function(event){
+				var name = $(event.target).closest("div").attr("data-name");
+				var login = $(event.target).closest("div").attr("data-login");
+				app.githubApi.getIssues({
+					name:name,
+					login:login,
+					state:"closed"
+				}).pipe(function(json){
+					brite.display("GithubIssues",$(".tab-content"),{issues:json.result,name:name,login:login,issueState:"closed"});
+				});
+			}
 		}
 	});
 })();
