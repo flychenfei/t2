545adebe5a6c649272370c038b72585c17b280e1
flymyao <flymyao@gmail.com>
2015-11-26 18:05:09 +0800

    #305 [Foursquare] User's avatar is not correct

--- src/main/webapp/js/FoursquareUserInfo.js
+++ src/main/webapp/js/FoursquareUserInfo.js
@@ -1,21 +1,19 @@
-(function(){
-	brite.registerView("FoursquareUserInfo",{emptyParent:true, parent:".FoursquareScreen-content"},{
-		create:function(data,config){
+(function () {
+    brite.registerView("FoursquareUserInfo", {emptyParent: true, parent: ".FoursquareScreen-content"}, {
+        create: function (data, config) {
             return showUserInfo.call(this);
-		},
-		events:{
+        },
+        events: {}
+    });
 
-		}
-	});
-
-    function showUserInfo(){
+    function showUserInfo() {
         var dfd = $.Deferred();
-        app.foursquareApi.getUserInfo().done(function(result){
-
-            if(result.result.meta.code=='200'){
-               var html =  app.render("tmpl-FoursquareUserInfo",result.result.result);
-               console.log(html)
-               dfd.resolve(html);
+        app.foursquareApi.getUserInfo().done(function (result) {
+            if (result.result.meta.code == '200') {
+                var photo = JSON.parse(result.result.result.photo);
+                result.result.result.photo = photo;
+                var html = app.render("tmpl-FoursquareUserInfo", result.result.result);
+                dfd.resolve(html);
             }
         });
         return dfd.promise();
