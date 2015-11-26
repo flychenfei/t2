4e4002053d083dfe0cc57c64bd678441b57cbcd7
wgqing <905867322@qq.com>
2015-11-26 14:04:21 +0800

    #304 [Folder IMAP] in component CreateFolder do code clean, add comments and format the codes

--- src/main/webapp/js/CreateFolder.js
+++ src/main/webapp/js/CreateFolder.js
@@ -3,6 +3,7 @@
 
     /**
      * Component: CreateFolder
+     * Description: for create folder view
      */
     (function ($) {
 
@@ -10,14 +11,15 @@
             parent:".MainScreen",
             emptyParent:false
         }, {
+            // --------- View Interface Implement--------- //
             create:function (data, config) {
-            	var view = this;
-            	data = data || {};
+                var view = this;
+                data = data || {};
                 if(data) {
-                    this.id = data.id;
+                    view.id = data.id;
                 }
                 if(data.type){
-                	view.type = data.type;
+                    view.type = data.type;
                 }
                 var html = app.render("tmpl-CreateFolder",data||{});
                 var $e = $(html);
@@ -26,13 +28,13 @@
             postDisplay:function (data, config) {
                 var view = this;
                 var $e = view.$el;
-                var mainScreen = view.mainScreen = $e.bComponent("MainScreen");
+                view.mainScreen = $e.bComponent("MainScreen");
                 $e.find("form").find("input[type=text]").focus();
             },
 
             close:function () {
-                var $e = this.$el;
-                $e.bRemove();
+                var view = this;
+                view.$el.bRemove();
             },
 
             submit:function () {
@@ -45,11 +47,11 @@
                     input.focus();
                     input.closest("div").addClass("has-error").find("span").html("Please enter valid folder name.");
                 } else {
-                	if(view.type == 'rest'){
-	                    dfd = app.googleApi.saveLabelRest({id:view.id, name: input.val()});
-                	}else{
-                		dfd = app.googleApi.saveFolder({oldName:view.id, name: input.val()});
-                	}
+                    if(view.type == 'rest'){
+                        dfd = app.googleApi.saveLabelRest({id:view.id, name: input.val()});
+                    }else{
+                        dfd = app.googleApi.saveFolder({oldName:view.id, name: input.val()});
+                    }
                     dfd.done(function (extraData) {
                         setTimeout((function () {
                             $(document).trigger("DO_REFRESH_FOLDERS");
@@ -59,13 +61,18 @@
 
                 }
             },
+            // --------- /View Interface Implement--------- //
 
+            // --------- Events--------- //
             events:{
+                //event for the Create button to create folder
                 "btap; .createFolderBtn":function () {
                     var view = this;
                     var $e = view.$el;
                     view.submit();
                 },
+
+                // keydown event for close or submit
                 "keydown": function (e) {
                     var view = this;
                     if (e.keyCode == 27) {
@@ -74,11 +81,14 @@
                         view.submit();
                     }
                 },
+
+                // event for Cancel button
                 "btap; .cancelBtn":function () {
                     var view = this;
                     view.close();
                 }
             }
+            // --------- /Events--------- //
         })
     })(jQuery);
 })();
