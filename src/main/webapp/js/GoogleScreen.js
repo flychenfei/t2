e96db4d852c937af54757b5ecb3df5d95ce02b8a
wgqing <905867322@qq.com>
2015-12-22 13:44:45 +0800

    #410 [GoogleScreem] clean and format the codes, remove unused codes and add comments

--- src/main/webapp/js/GoogleScreen.js
+++ src/main/webapp/js/GoogleScreen.js
@@ -6,6 +6,8 @@
 	 */
     (function ($) {
         brite.registerView("GoogleScreen",  {parent:".MainScreen-main",emptyParent:true}, {
+
+            // --------- View Interface Implement--------- //
             create:function (data, config) {
                 var $html = app.render("tmpl-GoogleScreen");
                 var $e = $($html);
@@ -16,55 +18,61 @@
                 var $e = view.$el;
                 brite.display("GoogleMails");
             },
-            events:{
-              "btap;.nav li":function(e){
-                var view = this;
-                var $e = view.$el;
-                var $li = $(e.currentTarget);
-                $e.find("li").removeClass("active");
-                $li.addClass("active");
-                
-                var menu = $li.attr("data-nav");
-                if(menu == "contacts"){
-                  brite.display("GoogleContacts");
-                }else if(menu == "groups"){
-                  brite.display("GoogleGroups");
-                }else if(menu == "calendar-events"){
-                  brite.display("GoogleCalendarEvents");
-                }else if(menu == "calendars"){
-                  brite.display("GoogleCalendars");
-                }else if(menu == "mails"){
-                  brite.display("GoogleMails");
-                }else if(menu == "mails_rest"){
-                  brite.display("GoogleMailsRest");
-                }else if(menu == "folders"){
-                  brite.display("GoogleFolders");
-                }else if(menu == "folders_rest"){
-                  brite.display("GoogleFoldersRest");
-                }else if(menu == "gmail_analytics"){
-                  brite.display("GoogleGmailAnalytics");
-                }else if(menu == "actions"){
-                    var list = [
-                        {name:"sendMail",label:"Send Mail"},
-                        {name:"sendMailRest",label:"Send Mail Via Rest"},
-                        {name:"createGroup",label:"Create Group"},
-                        {name:"createContact",label:"Create Contact"},
-                        {name:"searchEmail",label:"Search Email"},
-                        {name:"searchContact",label:"Search Contact"}
-                    ];
-                    brite.display("Dropdown",null,{$target:$li,list:list});
-                    $li.find("i").removeClass("glyphicon glyphicon-chevron-down").addClass("glyphicon glyphicon-chevron-up");
-                }else if(menu == "drive"){
-                	var list = [
-                                {name:"file",label:"Files"},
-                                {name:"trash",label:"Trash"}
-                            ];
-                    brite.display("Dropdown",null,{$target:$li,list:list});
-                    $li.find("i").removeClass("glyphicon glyphicon-chevron-down").addClass("glyphicon glyphicon-chevron-up");
-                }
-              }
-            },
+            // --------- /View Interface Implement--------- //
 
+            // --------- Events--------- //
+            events:{
+                // click event for each menu nav
+                "btap;.nav li":function(e){
+                    var view = this;
+                    var $e = view.$el;
+                    var $li = $(e.currentTarget);
+                    $e.find("li").removeClass("active");
+                    $li.addClass("active");
+
+                    var menu = $li.attr("data-nav");
+                    if(menu == "contacts"){
+                      brite.display("GoogleContacts");
+                    }else if(menu == "groups"){
+                      brite.display("GoogleGroups");
+                    }else if(menu == "calendar-events"){
+                      brite.display("GoogleCalendarEvents");
+                    }else if(menu == "calendars"){
+                      brite.display("GoogleCalendars");
+                    }else if(menu == "mails"){
+                      brite.display("GoogleMails");
+                    }else if(menu == "mails_rest"){
+                      brite.display("GoogleMailsRest");
+                    }else if(menu == "folders"){
+                      brite.display("GoogleFolders");
+                    }else if(menu == "folders_rest"){
+                      brite.display("GoogleFoldersRest");
+                    }else if(menu == "gmail_analytics"){
+                      brite.display("GoogleGmailAnalytics");
+                    }else if(menu == "actions"){
+                        var list = [
+                            {name:"sendMail",label:"Send Mail"},
+                            {name:"sendMailRest",label:"Send Mail Via Rest"},
+                            {name:"createGroup",label:"Create Group"},
+                            {name:"createContact",label:"Create Contact"},
+                            {name:"searchEmail",label:"Search Email"},
+                            {name:"searchContact",label:"Search Contact"}
+                        ];
+                        brite.display("Dropdown",null,{$target:$li,list:list});
+                        $li.find("i").removeClass("glyphicon glyphicon-chevron-down").addClass("glyphicon glyphicon-chevron-up");
+                    }else if(menu == "drive"){
+                        var list = [
+                            {name:"file",label:"Files"},
+                            {name:"trash",label:"Trash"}
+                        ];
+                        brite.display("Dropdown",null,{$target:$li,list:list});
+                        $li.find("i").removeClass("glyphicon glyphicon-chevron-down").addClass("glyphicon glyphicon-chevron-up");
+                    }
+                }
+            },
+            // --------- /Events--------- //
+
+           // --------- Document Events--------- //
             docEvents:{
                 "DO_ON_DROPDOWN_CLOSE":function(){
                     var view = this;
@@ -74,6 +82,7 @@
                     $li.find("i").removeClass("glyphicon glyphicon-chevron-up").addClass("glyphicon glyphicon-chevron-down");
                     $Li.find("i").removeClass("glyphicon glyphicon-chevron-up").addClass("glyphicon glyphicon-chevron-down");
                 },
+
                 "DO_ON_DROP_DOWN_CLICK":function(event, name) {
                     switch (name) {
                         case "sendMail":
@@ -127,19 +136,14 @@
                                 }});
                             break;
                         case "file":brite.display("GoogleDriveFiles");
-                        	break;
+                            break;
                         case "trash":brite.display("GoogleDriveTrash");
-                        	break;
+                            break;
                         default:
                     }
                 }
-            },
-
-            daoEvents:{
             }
-            
+            // --------- /Document Events--------- //
         });
     })(jQuery);
-
-
 })();
